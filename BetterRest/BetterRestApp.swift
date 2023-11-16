8eb25d14f2524c2a9b957121c5a336cf62f29aac
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-11-16 19:47:41 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Вадим Исламов on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
